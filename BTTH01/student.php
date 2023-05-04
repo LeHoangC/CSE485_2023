6f2c19187991b09d4f356880a9aa796f4a6df829
LeHoangC <lehoangcuong0202@gmail.com>
2023-05-04 14:35:25 +0700

    cuong

---
+++ BTTH01/student.php
@@ -0,0 +1,41 @@
+<?php
+class Student
+{
+    public $firstName;
+    public $lastName;
+
+    public function __construct($firstName, $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+
+    public function sayHello()
+    {
+        echo 'Hello, my name is ' . $this->firstName . ' and I am ' . $this->lastName . ' years old.';
+    }
+
+    public function readfile()
+    {
+
+        // $filename = './file.txt';
+        // $myfile = fopen($filename, "r");
+
+        // $contents = fread($myfile, filesize($filename)); //đọc file
+
+        // $convert = json_encode($contents);
+
+        // echo $convert;
+
+        // // đóng file
+        // fclose($myfile);
+
+        $lines = file('file.txt');
+
+        echo $lines;
+    }
+}
+
+$student1 = new Student('le', 'cuong');
+
+$student1->readfile();
